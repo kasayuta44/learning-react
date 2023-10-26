dae5e72eb3c905599ad011b0a33424dceaf38d44
kasayuta44 <67297439+kasayuta44@users.noreply.github.com>
2023-10-26 13:55:41 -0700

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -72,19 +72,88 @@ function MyButton() {
 }
 
 /*Tic-Tac-Toe stuff */
-function MySquare() {
+
+function MySquare({value, onSquareClick}) {
   return (
-    <button className="square">
-      X
+    <button className="square" onClick={onSquareClick}>
+      {value}
     </button>
   );
+}
+
+function MyBoard() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext)
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player :" + (xIsNext ? "X": "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <MySquare value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <MySquare value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <MySquare value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <MySquare value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <MySquare value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <MySquare value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <MySquare value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <MySquare value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <MySquare value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;  
 }
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my app!</h1>
-      <MySquare />
+      <MyBoard />
     </div>
     
   );
